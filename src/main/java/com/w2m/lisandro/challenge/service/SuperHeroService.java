d8c66520835b4131f802ce8b899a7a14538d79f9
lisandromedrano <lisandro.medrano@gmail.com>
2021-05-09 19:04:17 -0300

    Added cache

--- src/main/java/com/w2m/lisandro/challenge/service/SuperHeroService.java
+++ src/main/java/com/w2m/lisandro/challenge/service/SuperHeroService.java
@@ -3,21 +3,24 @@ package com.w2m.lisandro.challenge.service;
 import com.w2m.lisandro.challenge.model.SuperHero;
 import com.w2m.lisandro.challenge.repository.SuperHeroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@CacheConfig(cacheNames={"superheroes"})
 public class SuperHeroService {
     @Autowired
     private SuperHeroRepository superHeroRepository;
 
+    @Cacheable
     public List<SuperHero> findAll(){
         return superHeroRepository.findAll();
     }
 
     public SuperHero findById(Integer id){
-        //TODO: test returning empty
         return superHeroRepository.findById(id).get();
     }
 
@@ -43,6 +46,7 @@ public class SuperHeroService {
         superHeroRepository.deleteById(id);
     }
 
+    @Cacheable
     public List<SuperHero> findByName(String name){
         return superHeroRepository.findByNameContainingIgnoreCase(name);
     }
